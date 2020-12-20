93f7236d1146316a06438d1565528a95b3969f6a
Betty Ho <bettyvisualtech@gmail.com>
2020-12-20 19:36:01 +0000

    conditions

--- conditionals.js
+++ conditionals.js
@@ -9,8 +9,8 @@ if (cold) {
   console.log("Make sure you pick out a scarf!");
 }
 
-console.log("Now you're ready to go outside!");
 
+console.log("Now you're ready to go outside!");
 
 const temperature = 30;
 
